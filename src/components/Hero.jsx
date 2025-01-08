f3530bd068959f29f80266255bc2883d8a2ca0cc
Vladislav <baiborodov333@mail.ru>
2025-01-08 18:39:49 +0500

    product_set

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-
+import {assets} from '../assets/frontend_assets/assets'
 
 const Hero = () => {
   return (
@@ -9,13 +9,14 @@ const Hero = () => {
 					<p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
 					<p className='font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
 				</div>
-				<h1 className='text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arrivals</h1>
+				<h1 className='text-3xl sm:py-3 lg:text-5xl prata-regular leading-relaxed'>Latest Arrivals</h1>
 				<div className='flex items-center gap-2'>
 					<p className='font-semibold text-sm md:text-base'>SHOP NN</p>
 					<p className='w-8 md:11 h-[1px] bg-[#414141]'></p>
 				</div>
 			</div>
 		</div>
+		<img className='w-full sm:w-1/2' src={assets.hero_img} alt="" />
 	</div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import ShopContextProvider from "./context/ShopContext.jsx";
 
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <ShopContextProvider>
       <App />
+    </ShopContextProvider>
   </BrowserRouter>
-
-
-)
+);
