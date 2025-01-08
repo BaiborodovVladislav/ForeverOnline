15d662a10d11ee444e2b4d0053cba97126d877f1
Vladislav <baiborodov333@mail.ru>
2025-01-08 19:54:51 +0500

    footer_st

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
+import Bestseller from '../components/Bestseller'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
+import NewsLetterBox from '../components/NewsLetterBox'
+import OurPolicy from '../components/OurPolicy'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
       <LatestCollection/>
+      <Bestseller/>
+      <OurPolicy/>
+      <NewsLetterBox/>
     </div>
   )
 }
